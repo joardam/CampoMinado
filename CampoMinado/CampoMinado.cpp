f98856f943103ff703bcfc93f54f718ffdf50769
joardam <pedrofdias@live.com>
2023-08-27 10:06:18 -0300

    Gerando as bombas e casas

--- CampoMinado/CampoMinado.cpp
+++ CampoMinado/CampoMinado.cpp
@@ -22,14 +22,37 @@ int main()
 	for (int i = 1; i <= 10; i++)
 		for (int j = 1; j <= 10; j++) {
 			sgrid[i][j] = 10;
+
+			if (rand() % 5 == 0) grid[i][j] = 9;
+			else grid[i][j] = 0;
 		}
+
+	for (int i = 1; i <= 10; i++)
+		for (int j = 1; j <= 10; j++) {
+			int n = 0;
+			if (grid[i][j] == 9) continue;
+
+			if (grid[i + 1][j] == 9) n++;
+			if (grid[i - 1][j] == 9) n++;
+			if (grid[i][j + 1] == 9) n++;
+			if (grid[i][j - 1] == 9) n++;
+
+			if (grid[i + 1][j + 1] == 9) n++;
+			if (grid[i + 1][j - 1] == 9) n++;
+			if (grid[i - 1][j + 1] == 9) n++;
+			if (grid[i - 1][j - 1] == 9) n++;
+
+			grid[i][j] = n;
+		}
+
+
 
 	while (app.isOpen())
 	{
-		Event eve;
-		while (app.pollEvent(eve))
+		Event event;
+		while (app.pollEvent(event))
 		{
-			if (eve.type == Event::Closed)
+			if (event.type == Event::Closed)
 				app.close();
 
 		}
@@ -37,9 +60,17 @@ int main()
 
 		for (int i = 1; i <= 10; i++)
 			for (int j = 1; j <= 10; j++) {
-				sprite.setTextureRect(IntRect(sgrid[i][j] * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE));
-				sprite.setPosition(i * SPRITE_SIZE, j * SPRITE_SIZE);
-				app.draw(sprite);
+
+				for (int j = 1; j <= 10; j++) {
+
+					sgrid[i][j] = grid[i][j];
+
+
+
+					sprite.setTextureRect(IntRect(sgrid[i][j] * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE));
+					sprite.setPosition(i * SPRITE_SIZE, j * SPRITE_SIZE);
+					app.draw(sprite);
+				}
 			}
 	
 
